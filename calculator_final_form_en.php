9b8a1fd179b78635900c11aaccbdcfe8c78b13ff
sweetcherryboii <thomasclarisse777@gmail.com>
2025-02-18 22:48:24 +0100

    Add files via upload

---
+++ calculator_final_form_en.php
@@ -0,0 +1,78 @@
+<?php
+
+$method = readline("what poeration would you like to perform? (+ , -, *, /, %)");
+
+if ($method == "+") {
+    $first = readline("first number?");
+    if (!is_numeric($first)) {
+        echo "invalid input, the first number isn't a number!";
+    } else {
+        $last = readline("second number?");
+        if (!is_numeric($last)) {
+            echo "invalid input, the second number isn't a number!";
+        } else {
+            $equals = (intval($first) + intval($last));
+            echo "" . $equals . "";
+        }
+    }
+} elseif ($method == "-") {
+    $first = readline("first number?");
+    if (!is_numeric($first)) {
+        echo "invalid input, the first number isn't a number!";
+    } else {
+        $last = readline("second number?");
+        if (!is_numeric($last)) {
+            echo "invalid input, the second number isn't a number!";
+        } else {
+            $equals = (intval($first) - intval($last));
+            echo "" . $equals . "";
+        }
+    }
+} elseif ($method == "*") {
+    $first = readline("first number?");
+    if (!is_numeric($first)) {
+        echo "invalid input, the first number isn't a number!";
+    } else {
+        $last = readline("second number?");
+        if (!is_numeric($last)) {
+            echo "invalid input, the second number isn't a number!";
+        } else {
+            $equals = (intval($first) * intval($last));
+            echo "" . $equals . "";
+        }
+    }
+} elseif ($method == "/") {
+    $first = readline("first number?");
+    if (!is_numeric($first)) {
+        echo "invalid input, the first number isn't a number!";
+    } else {
+        $last = readline("second number?");
+        if (!is_numeric($last)) {
+            echo "invalid input, the second number isn't a number!";
+        } else {
+            if (intval($last) == 0) {
+                echo "dividing by zero is impossible... sorry!";
+            } else {
+                $equals = (intval($first) / intval($last));
+                echo "" . $equals . "";
+            }
+        }
+    }
+} elseif ($method == "%") {
+    $first = readline("first number?");
+    if (!is_numeric($first)) {
+        echo "invalid input, the first number isn't a number!";
+    } else {
+        $last = readline("second number?");
+        if (!is_numeric($last)) {
+            echo "invalid input, the second number isn't a number!";
+        } else {
+            $equals = (intval($first) % intval($last));
+            echo "" . $equals . "";
+        }
+    }
+} else {
+    echo "invalid operation! ";
+}
+
+?>
